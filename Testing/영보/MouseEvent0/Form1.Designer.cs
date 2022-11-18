67098338affa8e0ad0f016c81654562f7b394154
gyb357 <gyb357@naver.com>
2022-11-18 14:58:51 +0900

    no message

---
+++ Testing/영보/MouseEvent0/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace MouseEvent0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.userControl11 = new MouseEvent0.UserControl1();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(73, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 28);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(194, 99);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 18);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(194, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Form1.cs Area";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(73, 217);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(112, 22);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "속도 조절";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.userControl11.Location = new System.Drawing.Point(501, 0);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(500, 500);
+            this.userControl11.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.userControl11);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseLeave += new System.EventHandler(this.Form1_MouseLeave);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private UserControl1 userControl11;
+    }
+}
+
---
+++ Testing/영보/MouseEvent0/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseEvent0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        // 마우스 관련 함수 정의
+        public const UInt32 SPI_SETMOUSESPEED = 0x0071;
+        [DllImport("User32.dll")]
+        static extern Boolean SystemParametersInfo(
+            UInt32 uiAction,
+            UInt32 uiParam,
+            UInt32 pvParam,
+            UInt32 fWinIni);
+
+
+
+        // 마우스 속도 조정
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                SystemParametersInfo(SPI_SETMOUSESPEED, 0, 1, 0);
+            }
+            else
+            {
+                SystemParametersInfo(SPI_SETMOUSESPEED, 0, 10, 0);
+            }
+        }
+        /*
+         * SystemParametersInfo(SPI_SETMOUSESPEED, 0, 속도, 0);
+         * 
+         * "속도" 매개변수에 정수 값 삽입
+         * 
+         * 기본속도 = 10
+         */
+        /*
+         * 폼(Form1.cs Area) 위에서 마우스를 움직이면 이상하게 움직임
+         * 유저 컨트롤(UserControl1.cs Area) 위에선 땐 잘 움직임
+         * "마우스 활동 영역 제한" 함수와 뭔가 충돌이 있어서 그런 것 같음
+         * 
+         * 프로그램 종료 시 마우스 속도를 다시 원상복구 하는 기능이 있어야 함
+         */
+
+
+
+        // 마우스 위치 조정
+        private void Form1_MouseLeave(object sender, EventArgs e)
+        {
+            /*
+            int x = 100;
+            int y = 100;
+
+            Cursor.Position = new Point(x, y);
+            */
+        }
+
+
+
+        // 마우스 활동 영역 제한
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // 현재 마우스 xy좌표
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+
+
+            // Handle, Clip, Rectangle 이용
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(x, y);
+            Cursor.Clip = new Rectangle(this.Location, this.Size);
+
+
+            // 마우스 위치 출력
+            textBox1.Text = x.ToString();
+            textBox2.Text = y.ToString();
+        }
+
+
+
+        // 종료 버튼
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        // 마우스 모양 변경
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.Hand;
+        }
+
+
+
+        // 화면 중앙값
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 사용자 화면 중앙 좌표값
+            int x = Screen.PrimaryScreen.Bounds.Width / 2 - this.Size.Width / 2;
+            int y = Screen.PrimaryScreen.Bounds.Height / 2 - this.Size.Height / 2;
+
+
+            // 폼, 마우스를 화면 중앙에 위치
+            this.Location = new Point(x, y);   // 폼
+            Cursor.Position = new Point(x, y); // 마우스
+        }
+    }
+}
