7480bfe3472a06f0889ec965c8359793e3f72e5b
gguip <gguip7554@naver.com>
2022-12-01 21:32:23 +0900

    0.0.1
    버전명 붙이고
    1~3스테이지까지 이미지랑 그려서 연결시켜 놓고
    기본 화면 흰색으로 바꾸고 뒤로가기 버튼 그림으로 만들고
    적당히 중앙에 정렬하게 했음.

---
+++ Main 0.0.1/마우스피하기/MTAG.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace UserControl0
+{
+    public partial class MTAG : Form
+    {
+        public static MTAG form1;
+        public MTAG()
+        {
+            InitializeComponent();
+            
+            
+            form1 = this;
+
+
+            // 유저 컨트롤 배열
+            UserControl[] userControl = new UserControl[]
+            {
+                main1,         how_to_play1,
+                select_stage1, select_level1, stage_level1,
+                game_success1, game_over1,    game_pause1
+            };
+
+
+            // 유저 컨트롤 매개변수
+            Point   point      = new Point(0, 0);
+            Size    clientSize = new Size(ClientSize.Width, ClientSize.Height - 25);
+            Boolean visible    = false;
+
+
+            // 유저 컨트롤 초기화
+            for (int i = 0; i < userControl.Length; i++)
+            {
+                InitializeUserControl(userControl[i], point, clientSize, visible);
+            }
+
+
+            // 첫 화면으로 main1.cs (메인 화면) 출력
+            main1.Visible = true;
+        }
+
+
+
+        // 클래스 객체
+        MouseEvent mouseEvent = new MouseEvent();
+
+
+
+        // 요소
+        public int stage = 0; // 현재 스테이지
+        public int level = 0; // 현재 레벨
+        /*
+         * 각 스테이지와 레벨을 차별화 할 수 있는 요소
+         * -> 한개의 유저 컨트롤로 모두 다른 스테이지와 레벨 표현이 가능
+         * 
+         * 
+         * ex)
+         *     1. 스테이지 레벨마다 사용할 이미지를 참조에 삽입
+         * 
+         *     2. stage, level 값에 따라
+         *        해당 스테이지와 레벨에 맞는 이미지를 출력
+         */
+        public int   state      = 0;           // 현재 게임 상태 (현재 화면이 stage_level1.cs (게임 화면) 일 때 1 : 0)
+        public int   stageCount = 3;           // 스테이지 개수
+        public int   levelCount = 6;           // 레벨 개수
+        public Point mousePoint = new Point(); // 마우스 위치
+        public Color mouseColor = new Color(); // 마우스 위치 색상
+
+
+
+        // UserControl 초기화
+        private void InitializeUserControl(UserControl uc, Point p, Size s, Boolean b)
+        {
+            uc.Location = p;
+            uc.Size     = s;
+            uc.Visible  = false;
+        }
+
+
+
+        // UserControl 이동
+        public void UserControlVisible(UserControl uc1, UserControl uc2)
+        {
+            uc1.Visible = true;
+            uc2.Visible = false;
+        }
+
+
+
+        // 타이머
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // state 값이 1일 때만 작동시켜 틱당 연산을 줄일 계획
+            if (true)
+            {
+                // 커서 위치의 색상
+                mouseColor = mouseEvent.GetCursorColor(Cursor.Position);
+
+
+                // 노란색일 때
+                if (mouseColor == Color.FromArgb(255, 242, 0))
+                {
+                    // game_success1.cs (클리어 화면) 로 이동
+                    UserControlVisible(game_success1, stage_level1);
+                }
+
+
+                // 빨간색일 때
+                if (mouseColor == Color.FromArgb(237, 28, 36))
+                {
+                    // game_over1.cs (게임 오버 화면) 로 이동
+                    UserControlVisible(game_over1, stage_level1);
+                }
+            }
+
+
+            // 디버그
+            label1.Text = "color: " + mouseColor.ToString();
+            label2.Text = "stage: " + stage.ToString();
+            label3.Text = "level: " + level.ToString();
+            label4.Text = "state: " + state.ToString();
+        }
+    }
+}
---
+++ Main 0.0.1/마우스피하기/UserControl/game_over.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace UserControl0
+{
+    partial class game_over
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(350, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "게임 오버";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(250, 300);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "다시하기";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label_Click);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(450, 300);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "뒤로가기";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label_Click);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label_MouseMove);
+            // 
+            // game_over
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "game_over";
+            this.Size = new System.Drawing.Size(800, 600);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
