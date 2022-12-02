3d9858d6782a5482e8afd48c3a63c0bde6b0be78
gguip <gguip7554@naver.com>
2022-12-03 06:00:57 +0900

    0.0.2
    디자인 개선, 2-3, 3-3 스테이지 만듦

---
+++ Main 0.0.2/마우스피하기/MTAG.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace UserControl0
+{
+    public partial class MTAG : Form
+    {
+        public static MTAG form1;
+        public MTAG()
+        {
+            InitializeComponent();
+
+
+            form1 = this;
+
+
+            // 유저 컨트롤 배열
+            UserControl[] userControl = new UserControl[]
+            {
+                main1,         how_to_play1,
+                select_stage1, select_level1, stage_level1,
+                game_success1, game_over1,    game_pause1
+            };
+
+
+            // 유저 컨트롤 매개변수
+            Point point = new Point(0, 0);
+            Size clientSize = new Size(ClientSize.Width, ClientSize.Height - 25);
+            Boolean visible = false;
+
+
+            // 유저 컨트롤 초기화
+            for (int i = 0; i < userControl.Length; i++)
+            {
+                InitializeUserControl(userControl[i], point, clientSize, visible);
+            }
+
+
+            // 첫 화면으로 main1.cs (메인 화면) 출력
+            main1.Visible = true;
+        }
+
+
+
+        // 클래스 객체
+        MouseEvent mouseEvent = new MouseEvent();
+
+
+
+        // 요소
+        public int stage = 0; // 현재 스테이지
+        public int level = 0; // 현재 레벨
+        /*
+         * 각 스테이지와 레벨을 차별화 할 수 있는 요소
+         * -> 한개의 유저 컨트롤로 모두 다른 스테이지와 레벨 표현이 가능
+         * 
+         * 
+         * ex)
+         *     1. 스테이지 레벨마다 사용할 이미지를 참조에 삽입
+         * 
+         *     2. stage, level 값에 따라
+         *        해당 스테이지와 레벨에 맞는 이미지를 출력
+         */
+        public int state = 0;           // 현재 게임 상태 (현재 화면이 stage_level1.cs (게임 화면) 일 때 1 : 0)
+        public int stageCount = 3;           // 스테이지 개수
+        public int levelCount = 6;           // 레벨 개수
+        public Point mousePoint = new Point(); // 마우스 위치
+        public Color mouseColor = new Color(); // 마우스 위치 색상
+
+
+
+        // UserControl 초기화
+        private void InitializeUserControl(UserControl uc, Point p, Size s, Boolean b)
+        {
+            uc.Location = p;
+            uc.Size = s;
+            uc.Visible = false;
+        }
+
+
+
+        // UserControl 이동
+        public void UserControlVisible(UserControl uc1, UserControl uc2)
+        {
+            uc1.Visible = true;
+            uc2.Visible = false;
+        }
+
+
+        int time = 0; // timer 지연
+        // 타이머
+        private void timer1_Tick(object sender, EventArgs e)
+        {          
+            time++;
+            // state 값이 1일 때만 작동시켜 틱당 연산을 줄일 계획
+            if (true)
+            {
+                // 커서 위치의 색상
+                mouseColor = mouseEvent.GetCursorColor(Cursor.Position);
+
+
+                // 노란색일 때
+                if (mouseColor == Color.FromArgb(255, 242, 0))
+                {
+                    // game_success1.cs (클리어 화면) 로 이동
+                    UserControlVisible(game_success1, stage_level1);
+                }
+
+
+                // 빨간색일 때
+                if (mouseColor == Color.FromArgb(237, 28, 36))
+                {
+                    // game_over1.cs (게임 오버 화면) 로 이동
+                    UserControlVisible(game_over1, stage_level1);
+                }
+            }
+
+
+            // 디버그
+            label1.Text = "color: " + mouseColor.ToString();
+            label2.Text = "stage: " + stage.ToString();
+            label3.Text = "level: " + level.ToString();
+            label4.Text = "state: " + state.ToString();
+
+            if (MTAG.form1.stage == 2 && MTAG.form1.level == 3)
+            {
+                MTAG.form1.stage_level1.label2.Visible = true;
+                MTAG.form1.stage_level1.button1.Visible = true;
+                MTAG.form1.stage_level1.button2.Visible = true;
+                MTAG.form1.stage_level1.button3.Visible = true;
+                MTAG.form1.stage_level1.button4.Visible = true;
+                MTAG.form1.stage_level1.button5.Visible = true;
+                MTAG.form1.stage_level1.label2.Location = new Point(350, 200);
+                MTAG.form1.stage_level1.label2.Text = "다음중 배가 고픈것은?";
+            }
+            else
+            {
+                MTAG.form1.stage_level1.label2.Visible = false;
+                MTAG.form1.stage_level1.button1.Visible = false;
+                MTAG.form1.stage_level1.button2.Visible = false;
+                MTAG.form1.stage_level1.button3.Visible = false;
+                MTAG.form1.stage_level1.button4.Visible = false;
+                MTAG.form1.stage_level1.button5.Visible = false;
+            }
+
+            Random rand = new Random();// pictureBox 랜덤으로 위치바꾸고 색바꿔서 깨는 스테이지 3-3
+            if (MTAG.form1.stage == 3 && MTAG.form1.level == 3)
+            {
+                int x = rand.Next(800);
+                int y = rand.Next(600);
+                int temp = rand.Next(8);
+                MTAG.form1.stage_level1.pictureBox2.Visible = true;
+
+                if (temp == 1 && time % 50 == 1)
+                {
+                    MTAG.form1.stage_level1.pictureBox2.Location = new Point(x - MTAG.form1.stage_level1.pictureBox2.Width, y - MTAG.form1.stage_level1.pictureBox2.Height);
+                    MTAG.form1.stage_level1.pictureBox2.BackColor = Color.FromArgb(255, 242, 0);
+                }
+                else if(time % 50 == 1)
+                {
+                    MTAG.form1.stage_level1.pictureBox2.Location = new Point(x - MTAG.form1.stage_level1.pictureBox2.Width, y - MTAG.form1.stage_level1.pictureBox2.Height);
+                    MTAG.form1.stage_level1.pictureBox2.BackColor = Color.FromArgb(237, 28, 36);
+                }
+            }
+            else
+            {
+                MTAG.form1.stage_level1.pictureBox2.Visible = false;
+            }
+            if(time == 50)time = 0;
+        }
+    }
+}
---
+++ Main 0.0.2/마우스피하기/UserControl/select_stage.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace UserControl0
+{
+    partial class select_stage
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(select_stage));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 15F);
+            this.label1.Location = new System.Drawing.Point(100, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stage 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(100, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_click);
+            this.button1.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button_MouseMove);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::UserControl0.Properties.Resources.stage_2__150__150_;
+            this.button2.Location = new System.Drawing.Point(325, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 150);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_click);
+            this.button2.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button_MouseMove);
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = global::UserControl0.Properties.Resources.stage_3__150__150_;
+            this.button3.Location = new System.Drawing.Point(550, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 150);
+            this.button3.TabIndex = 7;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_click);
+            this.button3.MouseLeave += new System.EventHandler(this.button_MouseLeave);
+            this.button3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::UserControl0.Properties.Resources.뒤로가기___복사본;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 470);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("굴림", 15F);
+            this.label2.Location = new System.Drawing.Point(325, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 48);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Stage 2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("굴림", 15F);
+            this.label3.Location = new System.Drawing.Point(550, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 48);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Stage 3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // select_stage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "select_stage";
+            this.Size = new System.Drawing.Size(800, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
