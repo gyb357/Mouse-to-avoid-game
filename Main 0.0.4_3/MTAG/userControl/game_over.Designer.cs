a68edd8cdc05ca86c75b33c334bface9b97709e7
gguip <gguip7554@naver.com>
2022-12-04 23:35:35 +0900

    0.0.4_3
    디자인 변경

---
+++ Main 0.0.4_3/MTAG/userControl/game_over.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MTAG
+{
+    partial class game_over
+    {
+        /// <summary> 
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 구성 요소 디자이너에서 생성한 코드
+
+        /// <summary> 
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.Image = global::MTAG.Properties.Resources.다시하기n;
+            this.label2.Location = new System.Drawing.Point(189, 324);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 73);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.Label_Click);
+            this.label2.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            this.label2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Label_MouseMove);
+            // 
+            // label3
+            // 
+            this.label3.Image = global::MTAG.Properties.Resources.선택화면으로n;
+            this.label3.Location = new System.Drawing.Point(459, 324);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 73);
+            this.label3.TabIndex = 2;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.Label_Click);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Label_MouseMove);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("굴림", 22F);
+            this.label1.Image = global::MTAG.Properties.Resources.게임_오버;
+            this.label1.Location = new System.Drawing.Point(243, 96);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 120);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // game_over
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "game_over";
+            this.Size = new System.Drawing.Size(800, 600);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Main 0.0.4_3/MTAG/userControl/main.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace MTAG
+{
+    public partial class main : UserControl
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            Label l = sender as Label;
+
+            if (l == label2) { Form1.form1.UserControlVisible(Form1.form1.select_stage1, this); }
+            if (l == label3) { Form1.form1.UserControlVisible(Form1.form1.how_to_play1, this); }
+            if (l == label4) { Form1.form1.Close(); }
+        }
+
+
+
+        private void Label_MouseMove(object sender, MouseEventArgs e)
+        {
+            Label l = sender as Label;
+            //Cursor.Current = Cursors.Hand;
+            if (l == label2)
+            {
+                label2.Image = Properties.Resources.게임_시작_p;
+            }
+            else if (l == label3)
+            {
+                label3.Image = Properties.Resources.게임_방법_p;
+            }
+            else if (l == label4)
+            {
+                label4.Image = Properties.Resources.끝내기_p;
+            }
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            Label l = sender as Label;
+            if (l == label2)
+            {
+                label2.Image = Properties.Resources.게임_시작_n;
+            }
+            else if (l == label3)
+            {
+                label3.Image = Properties.Resources.게임_방법_n;
+            }
+            else if (l == label4)
+            {
+                label4.Image = Properties.Resources.끝내기_n;
+            }
+        }
+    }
+}
